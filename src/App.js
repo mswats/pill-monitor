9f5a664d6810b621274cd122f0b264f13a0ddb54
mswats <melissasdaa@gmail.com>
2021-01-16 16:04:05 -0500

    test

--- src/App.js
+++ src/App.js
@@ -9,17 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-          </Card.Text>
-          <Button color="success" size="lg">Go somewhere</Button>
-        </Card.Body>
-      </Card>
+      "Test"
     </div>
   );
 }
